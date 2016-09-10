47617421484196d70529ef1c3e9d8ec87f23cb3f
seanmurphy15 <seanduganmurphy@gmail.com>
2016-09-10 16:16:47 -0500

    Initial Commit

---
+++ PhotoBlitz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoBlitz
+//
+//  Created by Sean Murphy on 9/10/16.
+//  Copyright © 2016 Sean Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
