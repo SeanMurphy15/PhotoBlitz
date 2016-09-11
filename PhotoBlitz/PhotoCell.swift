df3a8d3ce94e79fc0520d19679c9af7e626d36b4
seanmurphy15 <seanduganmurphy@gmail.com>
2016-09-10 19:43:08 -0500

    cloud persistence and downloading handled

---
+++ PhotoBlitz/PhotoCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCell.swift
+//  PhotoBlitz
+//
+//  Created by Sean Murphy on 9/10/16.
+//  Copyright © 2016 Sean Murphy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCell: UICollectionViewCell{
+
+	@IBOutlet weak var thumbnailImageView: UIImageView!
+	
+}
---
+++ PhotoBlitz/PhotoCollectionViewController.swift
@@ -0,0 +1,111 @@
+//
+//  PhotoCollectionViewController.swift
+//  PhotoBlitz
+//
+//  Created by Sean Murphy on 9/10/16.
+//  Copyright © 2016 Sean Murphy. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+private let reuseIdentifier = "photoCell"
+
+
+class PhotoCollectionViewController: UICollectionViewController {
+
+	let storageRef = FIRStorage.storage().referenceForURL("gs://photoblitz-c9f3d.appspot.com")
+	var imageArray: [UIImage] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+			let images = storageRef.child("images/image")
+			// Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
+			images.dataWithMaxSize(4 * 1024 * 1024) { (data, error) -> Void in
+				if (error != nil) {
+					print(error)
+				} else {
+						self.imageArray.append(UIImage(data: data!)!)
+					self.collectionView?.reloadData()
+				}
+			}
+
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return imageArray.count
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! PhotoCell
+    
+       let photo = imageArray[indexPath.row]
+
+			for image in imageArray{
+
+				cell.thumbnailImageView.image = image
+
+			}
+
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+
+}
